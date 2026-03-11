b6f4e0866578c400cbcbcd0e89018070b7b7a0a6
Byte130 <rhuisman09@gmail.com>
2026-03-11 14:44:01 +0100

    feet
    de les eindigd bijna en brian gooned naar mommy asmr

--- Assets/Scripts/Play Button.cs
+++ Assets/Scripts/Play Button.cs
@@ -13,17 +13,6 @@ public class PlayButton : MonoBehaviour
     
     void Update()
     {
-       if (Input.GetMouseButtonDown(0))
-       {
-           Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-           RaycastHit hit;
-           if (Physics.Raycast(ray, out hit))
-           {
-               if (hit.collider.gameObject == Button)
-               {
-                   SceneManager.LoadScene("MainScene");
-               }
-           }
-        }
+
     }
 }
